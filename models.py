71207337f8c49ace217c162a481ce60f415151f2
umair-gujjar <engrumair_sabir@yahoo.com>
2016-04-06 15:18:56 +0500

    add fields in manufacturaing order

--- models.py
+++ models.py
@@ -15,6 +15,86 @@ class mrp_custom_expert(models.Model):
             ('NEW', 'New'),
             ('REPEAT', 'Repeat'),
             ],default='REPEAT', string="Job Type")
+	mo_lamination_type = fields.Selection([
+            ('mat', 'MAT'),
+            ('glass', 'Glass'),
+            ('varnish', 'Varnish'),
+            ],default='', string="Type of Lamination")
+
+	mo_mrp_mat = fields.Boolean(string='Mat')
+	mo_mrp_glass = fields.Boolean(string='Glass')
+	mo_mrp_varnish = fields.Boolean(string='Varnish')
+	mo_type_of_varnish = fields.Selection([
+            ('a', 'A'),
+            ('b', 'B'),
+            ],default='', string="Type of Varnish")
+	mo_salofeen_width = fields.Char(string='Salofeen Width')
+	mo_salofeen_height = fields.Char(string='Salofeen Height')
+	mo_paste_materials = fields.Selection([
+            ('a', 'A'),
+            ('b', 'B'),
+            ],default='', string="Paste Material")
+	mo_mrp_side_dropdown = fields.Selection([
+            ('a', 'By Hand'),
+            ('b', 'By Machine'),
+            ],default='', string="Side")
+	mo_mrp_crush = fields.Selection([
+            ('a', 'A'),
+            ('b', 'B'),
+            ],default='', string="Crush")
+	mo_mrp_pins = fields.Integer(string='Pins')
+	mo_mrp_reel = fields.Float(string='Reel')
+	mo_mrp_cutting = fields.Float(string='Cutting')
+	mo_mrp_cut_width = fields.Float(string='Cut Width')
+	mo_mrp_cut_height = fields.Float(string='Cut Height')
+	mo_mrp_type = fields.Selection([
+            ('a', 'Packets'),
+            ('b', 'Rolls'),
+            ],default='', string="Type")
+	mo_mrp_grams = fields.Float(string='Grams')
+	mo_mrp_fac = fields.Integer(string='Fac')
+	mo_mrp_nos = fields.Float(string='Nos')
+	mo_mrp_filter = fields.Char(string='Filter')
+	mo_p_mrp_nos = fields.Float(string='Nos')
+	mo_p_mrp_filter = fields.Char(string='Filter')
+	mo_p_mrp_ups = fields.Integer(string='UPS')
+	mo_p_mrp_brand = fields.Char(string='Brand')
+
+	mo_mrp_machine = fields.Char(string='Machine')
+	mo_mrp_grip = fields.Char(string='Grip')
+	mo_mrp_color = fields.Char(string='Color')
+	mo_mrp_quantity = fields.Float(string='Quantity')
+	mo_mrp_remarks = fields.Float(string='Remarks')
+	mo_mrp_remarks_description = fields.Text(string='Remarks')
+	mo_mrp_die_number = fields.Char(string='Die Number')
+	mo_mrp_die_type = fields.Selection([
+            ('a', 'Old'),
+            ('b', 'New'),
+            ],default='', string="Die Type")
+	mo_mrp_die_loc = fields.Char(string="Die Location")
+	mo_mrp_foiling = fields.Boolean(string='Foiling')
+	mo_mrp_rotry_salt = fields.Boolean(string='Rotry & Salt')
+	mo_mrp_imbosing = fields.Boolean(string='Imbosing')
+	mo_mrp_guilotine = fields.Boolean(string='Guilotine')
+	mo_mrp_screen = fields.Boolean(string='Screen')
+	mo_mrp_reel_size = fields.Float(string='Reel Size')
+	mo_mrp_cutting_size = fields.Float(string='Cutting Size')
+	mo_mrp_fact = fields.Float(string='Fact')
+	mo_mrp_pcs = fields.Float(string='PCS')
+	mo_mrp_ply = fields.Float(string='Ply')
+	mo_mrp_act_reel_size = fields.Float(string='Act Reel Size')
+
+	mo_mrp_ups_desc = fields.Char(string='UPS Desc')
+	mo_mrp_part_desc = fields.Char(string='Part Desc')
+	mo_mrp_sheets_qty = fields.Float(string='Sheets Quantity')
+	mo_mrp_job_qty = fields.Float(string='Job Quantity')
+	#mo_workbook_one_ids = fields.One2many('workbook_one','workbook_one_id')
+	#mo_workbook_two_ids = fields.One2many('workbook_two','workbook_two_id')
+
+	@api.onchange('bom_id')
+	def onchange_bom_id(self):
+		print 'nncvnnnxvncvnxcvnxvnnnnnnnnnnnnnnnnnnnnnnnnnnnnn'
+		print self.bom_id
 
 
 
@@ -76,6 +156,7 @@ class mrp_bom_custom_expert(models.Model):
             ('a', 'Old'),
             ('b', 'New'),
             ],default='', string="Die Type")
+	mrp_die_loc = fields.Char(string="Die Location")
 	mrp_foiling = fields.Boolean(string='Foiling')
 	mrp_rotry_salt = fields.Boolean(string='Rotry & Salt')
 	mrp_imbosing = fields.Boolean(string='Imbosing')
